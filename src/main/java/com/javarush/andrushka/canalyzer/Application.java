34be2e6a6d3831df2924771d0cdd294f9e27314e
victorandruscagit <victor.v.andrusca@gmail.com>
2025-02-06 14:47:35 +0200

    Dealing with actions(econding and decoding) in MainController.

--- src/main/java/com/javarush/andrushka/canalyzer/Application.java
+++ src/main/java/com/javarush/andrushka/canalyzer/Application.java
@@ -1,12 +1,25 @@
 package com.javarush.andrushka.canalyzer;
 
+import com.javarush.andrushka.canalyzer.controllers.MainController;
 import com.javarush.andrushka.canalyzer.entity.Result;
 
+import java.util.Arrays;
+
 public class Application {
-    public Application(String[] args) {
+    private MainController mainController;
+
+    public Application() {
+        mainController.doAction()
+        mainController = new MainController();
+
     }
 
-    public Result run() {
+    public Result run(String[] args) {
+        if (args.length > 0) {
+            String action = args[0];
+            String[] parameters = Arrays.copyOfRange(args, 1, args.length);
+            mainController.doAction(action, parameters);
+        }
         return null;
     }
 }
---
+++ src/main/java/com/javarush/andrushka/canalyzer/controllers/MainController.java
@@ -0,0 +1,12 @@
+package com.javarush.andrushka.canalyzer.controllers;
+
+import com.javarush.andrushka.canalyzer.entity.Result;
+
+public class MainController {
+    public Result
+    doAction( String  action, String [] args){
+        return null;
+
+
+    }
+}
