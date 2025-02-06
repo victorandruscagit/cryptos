4fdc385a865e4eebcdc71b697b30abb2e3e88ba0
victorandruscagit <victor.v.andrusca@gmail.com>
2025-02-06 13:34:06 +0200

    Refactored constants(just to play with git)

--- src/main/java/com/javarush/andrushka/canalyzer/constantsutil/Constants.java
+++ src/main/java/com/javarush/andrushka/canalyzer/constantsutil/Constants.java
@@ -1,9 +1,9 @@
 package com.javarush.andrushka.canalyzer.constantsutil;
 
 public class Constants {
-     private static final String rus = "йфяцычувскамепинртгоьшлщдзжбю";
-    private static final  String eng = "qazwsxedcrfvtgbyhnujmiklop";
-    private static final String digits = "0123456789";
-    private static final String  dMarks = "!@#$%^&*()_+-{}[]:;\"<,>.?/\\";
-    public static final String alphabet = rus + eng + rus.toLowerCase() + eng.toLowerCase() + digits + dMarks;
+     private static final String RUS = "йфяцычувскамепинртгоьшлщдзжбю";
+    private static final  String ENG = "qazwsxedcrfvtgbyhnujmiklop";
+    private static final String DIGITS = "0123456789";
+    private static final String D_MARKS = "!@#$%^&*()_+-{}[]:;\"<,>.?/\\";
+    public static final String ALPHABET = RUS + ENG + RUS.toLowerCase() + ENG.toLowerCase() + DIGITS + D_MARKS;
 }
