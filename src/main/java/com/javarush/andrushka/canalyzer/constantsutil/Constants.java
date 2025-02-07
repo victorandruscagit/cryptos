d41557beeb62c43fb9902b5c718f9682b55f7117
victorandruscagit <victor.v.andrusca@gmail.com>
2025-02-07 10:19:39 +0200

    Added text files and changed paths in Constants.java

--- src/main/java/com/javarush/andrushka/canalyzer/constantsutil/Constants.java
+++ src/main/java/com/javarush/andrushka/canalyzer/constantsutil/Constants.java
@@ -1,9 +1,14 @@
 package com.javarush.andrushka.canalyzer.constantsutil;
 
+import java.io.File;
+
 public class Constants {
-     private static final String RUS = "йфяцычувскамепинртгоьшлщдзжбю";
-    private static final  String ENG = "qazwsxedcrfvtgbyhnujmiklop";
+    private static final String RUS = "йфяцычувскамепинртгоьшлщдзжбю";
+    private static final String ENG = "qazwsxedcrfvtgbyhnujmiklop";
     private static final String DIGITS = "0123456789";
     private static final String D_MARKS = "!@#$%^&*()_+-{}[]:;\"<,>.?/\\";
     public static final String ALPHABET = RUS + ENG + RUS.toLowerCase() + ENG.toLowerCase() + DIGITS + D_MARKS;
+    public static final String TXT_FOLDER =
+            System.getProperty("user.dir") + File.separator +
+                    "text" + File.separator;
 }
