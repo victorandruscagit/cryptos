b974eea3ef40ac3097d2d920ed8d1ba3c64a6625
victorandruscagit <victor.v.andrusca@gmail.com>
2025-02-07 13:21:36 +0200

    Done with app's carcase

--- src/main/java/com/javarush/andrushka/canalyzer/commands/Decoder.java
+++ src/main/java/com/javarush/andrushka/canalyzer/commands/Decoder.java
@@ -1,10 +1,12 @@
 package com.javarush.andrushka.canalyzer.commands;
 
 import com.javarush.andrushka.canalyzer.entity.Result;
+import com.javarush.andrushka.canalyzer.entity.ResultCode;
 
-public class CommandEncoder implements Action{
+public class Decoder implements Action{
     @Override
     public Result exectute(String[] parameters) {
-        return null;
+
+        return new Result("decode all right", ResultCode.OK);
     }
 }
--- src/main/java/com/javarush/andrushka/canalyzer/commands/Encoder.java
+++ src/main/java/com/javarush/andrushka/canalyzer/commands/Encoder.java
@@ -1,10 +1,12 @@
 package com.javarush.andrushka.canalyzer.commands;
 
 import com.javarush.andrushka.canalyzer.entity.Result;
+import com.javarush.andrushka.canalyzer.entity.ResultCode;
 
-public class CommandDecodder implements Action{
+public class Encoder implements Action{
     @Override
     public Result exectute(String[] parameters) {
-        return null;
+
+         return new Result("encode all right", ResultCode.OK);
     }
 }
